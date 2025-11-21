a15cd29bbcc5092821eac2c34c731340a3f2fe8a
yunfeihit <yunfeihit@gmail.com>
2025-11-21 12:00:31 +0800

    submit version 1.0

--- script.js
+++ script.js
@@ -45,11 +45,10 @@ function GameBoard() {
     }
 
 
-    //Basic elements - Define the Combo to check whin or loose
+    //Basic elements - Define the Combo to check win or loose
     const winCheckCombos = [
         //the rows
         [board[0][0],board[0][1],board[0][2]],
-
         [board[1][0],board[1][1],board[1][2]],
         [board[2][0],board[2][1],board[2][2]],
         //the columns
@@ -62,10 +61,11 @@ function GameBoard() {
     ];
     
     //Return Function: the 'Getter'
+    //(return the 2D array board, not the object board)
     const getBoard = () => board;
 
-    //Return Function: show the board
-    const showBoard = () => {
+    //Return Function: display the board
+    const displayBoard = () => {
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
                 const gridItem = document.querySelector(`#board${i}${j}`);
@@ -96,7 +96,7 @@ function GameBoard() {
 
     return {
         getBoard,
-        showBoard,
+        displayBoard,
         placeMark,
         isCellFull,
         winCheckCombos,
@@ -112,7 +112,7 @@ function GameController (
 
     //initial board
     const board = GameBoard();
-    
+    // (list all empty cells in a flat array first)
     const players = [
         {
             name: playerName,
@@ -123,15 +123,16 @@ function GameController (
             token: 2
         }
     ];
-
     let activePlayer = players[0];
 
-    // (list all empty cells in a flat array first)
-    const emptyCellsArray = board.getBoard().flat().filter(el => el.getValue() === 0)  
+    //Inner Function: get empty cell array
+    //(It's important to use a function to get the emptyCellsArray istead of name the variable "emptyCellsArray", function can update the variable everytime call it)
+    const getEmptyCellsArray = () => 
+        board.getBoard().flat().filter(el => el.getValue() === 0); 
 
     //Inner Function: 'showPresentRound'
     const showPresentRound = () => {
-        board.showBoard();
+        board.displayBoard();
     };
 
     //Return Function: 'getActivePlayer'
@@ -141,12 +142,12 @@ function GameController (
     //(get the present board, return the pseudo AI move place: Cell / board[x][x])
     const getPseudoAIMove = () => { 
 
-        //1. Inner Method to find the Combo that the AI with last move to win, like [2, 2, 0]
+        //1. CallBack Method to find the Combo that the AI with last move to win, like [2, 2, 0]
         const if2_2_0 = (array3Els) => 
             array3Els.filter(el => el.getValue() === 2).length === 2 &&
             array3Els.filter(el => el.getValue() === 0).length === 1;
 
-        //2. Inner Method to find the Combo that the Gamer with last move to win, like [1,1,0]
+        //2. CallBack Method to find the Combo that the Gamer with last move to win, like [1,1,0]
         const if1_1_0 = (array3Els) => 
             array3Els.filter(el => el.getValue() === 1).length === 2 &&
             array3Els.filter(el => el.getValue() === 0).length === 1;
@@ -154,25 +155,26 @@ function GameController (
         const winMoveCombo = board.winCheckCombos.find(els => if2_2_0(els));
         const blockMoveCombo = board.winCheckCombos.find(els => if1_1_0(els));
 
-        //3. if above two conditions don't exist, find ramdom empty cell and place into it. 
+        //3. Check win combo and block combo first, if above two combos don't exist, find ramdom empty cell and place into it. 
    
+        const emptyCellsArray = getEmptyCellsArray();
         const theMovePlace = winMoveCombo  
             ? winMoveCombo.find(el => el.getValue() === 0) 
             : blockMoveCombo  
                 ? blockMoveCombo.find(el => el.getValue() === 0) 
                 : emptyCellsArray[Math.floor(Math.random() * emptyCellsArray.length)];
         
-        //(It's a Cell object is returned, like 'board[1][2]', which equal to a Cell object, Cell.row=1, Cell.column=2)
+        //(It's a Cell object being returned, like 'board[1][2]', which equal to a Cell object, Cell.row=1, Cell.column=2)
         return theMovePlace;
     }
 
     //Inner Function: 'returnWinnerToken'
     //(get the present board, check if there is a winner, return winner's token 1(gamer) or 2(AI) . if no winner, return 0)
     const returnWinnerToken = () => {
-        //(first: define the callback method to find an array has 3 consistent elements)
-        const findWinCombos = (arrayHas3Element) => {
-            const ifAll1 = arrayHas3Element.every(element => element.getValue() === 1);
-            const ifAll2 = arrayHas3Element.every(element => element.getValue() === 2);
+        //callback method: find an array has 3 same elements)
+        const findWinCombos = (ifArray3SameELes) => {
+            const ifAll1 = ifArray3SameELes.every(element => element.getValue() === 1);
+            const ifAll2 = ifArray3SameELes.every(element => element.getValue() === 2);
             if (ifAll1 || ifAll2) {
                 return true;
             } else {
@@ -188,48 +190,45 @@ function GameController (
         return winnerCombo[0].getValue();
     }
 
+    //Inner Function: initial game
+    // (clear the resultDisplay, clear the grid innerHTML, reset the board token to 0)
+    const initialGame = () => {
+        resultDisplay.textContent = "";
+
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                const gridItem = document.querySelector(`#board${i}${j}`);
+                gridItem.innerHTML = "";
+                //initial board[][]
+                //('board' is initialed in the closure of 'GameController', but 'board' means an object instead of the array, if you want to use the array of board, you should use 'board.getBoard')
+                const boardArray = board.getBoard();
+                boardArray[i][j].addToken(0);
+            }
+        }
+    }
+    
     //Inner Function: checkWin
     //(check if the game has a winner)
     const checkWin = () => {
-        console.log('running checkwin()....')
-
-
         //get the winner token: 1 or 2 or 0(no winner)
         const winnerToken = returnWinnerToken();
-        //define initial game function(ready to play a new round)
-        const initialGame = () => {
-            console.log('game is initialed')
-            resultDisplay.textContent = "";
 
-            for (let i = 0; i < 3; i++) {
-                for (let j = 0; j < 3; j++) {
-                    const gridItem = document.querySelector(`#board${i}${j}`);
-                    gridItem.innerHTML = "";
-                    //'board' is initialed in the closure of 'GameController', but 'board' means an object instead of the array, if you want to use the array of board, you should use 'board.getBoard'
-                    const boardArray = board.getBoard();
-                    boardArray[i][j].addToken(0);
-                }
-            }
-
-        }
-        // run the code: 
+        // running code: 
         if (winnerToken === 1) {
-            console.log('winner is gamer, find the winner token, about to show the result....')
-
             resultDisplay.textContent = `You beat the AI!`;
-            //add a onetime eventListener which can clear the board and the result display, so after there is an winner, if you click the board, the board will initial
+            //(add a onetime eventListener which can clear the board and the result display, so after if there is an winner, if you click the board, the board will initial)
+            //(It's important to use 'setTimeout()' instead of set the eventLisntener directally, because the browser will add the eventListener "before" the click, so when click grid, it will call "game.playOneRound" and this at the same time. So at the last move to vin, strange things will happen)
             setTimeout(() => {
                 boardDisplay.addEventListener('click', () => initialGame(), {once: true})
             }, 0);
             return true;
         } else if (winnerToken === 2) {
-            console.log('winner is AI, find the winner token, about to show the result....')
-
             resultDisplay.textContent = `Winner is AI!`;
             //same as above
             setTimeout(() => {
                 boardDisplay.addEventListener('click', () => initialGame(), {once: true})
-            }, 0);            return true;
+            }, 0);            
+            return true;
         } else {
             return false;
         }
@@ -237,10 +236,14 @@ function GameController (
 
     //Inner Function: Tie check
     const ifBoardIsFull = () => {
-        if (emptyCellsArray.length === 0) {return true}
+        if (getEmptyCellsArray().length === 0) {
+            setTimeout(() => {
+                boardDisplay.addEventListener('click', () => initialGame(), {once: true})
+            }, 0);
+            return true;
+        }
         else {return false;}
     }
-
 
     //MAIN FUNTION!
     //Return Function: 'playOneRound' 
@@ -250,7 +253,7 @@ function GameController (
 
         //1. Place the Gamer's Mark(if the place is not empty)
         if (board.isCellFull(row, column)) {
-            return false;
+            return;
         } else {
             board.placeMark(row, column, 1);
         }
@@ -259,14 +262,12 @@ function GameController (
         showPresentRound();   
 
         //3. judge whether there is a winner
+        //(this statement will run 'checkWin()' at the same time and get the return of it, that's important.)
         if (checkWin()) return;
 
         //check if it is a tie
         if (ifBoardIsFull()) {
-            resultDisplay.textContent = `It's a tie!`;
-            setTimeout(() => {
-                boardDisplay.addEventListener('click', () => initialGame(), {once: true})
-            }, 0);
+            resultDisplay.textContent = `It's a tie, try again!`;
             return;
         };
 
@@ -282,10 +283,6 @@ function GameController (
 
         //check if it is a tie
         if (ifBoardIsFull()) {
-            resultDisplay.textContent = `It's a tie!`;
-            setTimeout(() => {
-                boardDisplay.addEventListener('click', () => initialGame(), {once: true})
-            }, 0);
             return;
         };
 
